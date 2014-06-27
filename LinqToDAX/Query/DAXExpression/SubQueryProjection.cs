b7d940cd769ea4c26553c33e80b30033d98d4eb6
gyfarkas@gmail.com <gyfarkas@gmail.com>
2014-06-28 01:03:10 +0200

    subqueries new files

---
+++ LinqToDAX/Query/DAXExpression/SubQueryProjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace LinqToDAX.Query.DAXExpression
+{
+    /// <summary>
+    /// Decorator class for subordinate queries 
+    /// </summary>
+    internal class SubQueryProjection : DaxExpression
+    {
+        internal ProjectionExpression Exp { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubQueryProjection"/> class.
+        /// </summary>
+        /// <param name="proj">Projection to be decorated as subordinate</param>
+        internal SubQueryProjection(Type type, ProjectionExpression exp)
+            : base((ExpressionType)DaxExpressionType.SubQuery, type)
+        {
+            this.Exp = exp;
+        }
+    }
+}
